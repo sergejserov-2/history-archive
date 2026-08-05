d9d642492290035f50fdc016dde0f20c11f49ac0
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-05 22:52:55 +0300

    Create types.js

---
+++ js/api/types.js
@@ -0,0 +1,98 @@
+// ======================================
+// Types API
+// ======================================
+
+import {
+
+    collection,
+
+    getDocs,
+
+    doc,
+
+    getDoc
+
+}
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+
+    db
+
+}
+from "../../firebase.js";
+
+// ======================================
+// Get all types
+// ======================================
+
+export async function getTypes() {
+
+    const snapshot = await getDocs(
+
+        collection(
+            db,
+            "types"
+        )
+
+    );
+
+    return snapshot.docs.map(
+
+        doc => ({
+
+            id: doc.id,
+
+            ...doc.data()
+
+        })
+
+    );
+
+}
+
+// ======================================
+// Get one type
+// ======================================
+
+export async function getType(
+
+    typeId
+
+) {
+
+    if (!typeId) {
+
+        return null;
+
+    }
+
+    const snapshot = await getDoc(
+
+        doc(
+
+            db,
+
+            "types",
+
+            typeId
+
+        )
+
+    );
+
+    if (!snapshot.exists()) {
+
+        return null;
+
+    }
+
+    return {
+
+        id: snapshot.id,
+
+        ...snapshot.data()
+
+    };
+
+}
