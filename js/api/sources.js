9613e822fb6310a42ded21b40a988c74fca7fa08
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-05 21:31:46 +0300

    Create sources.js

---
+++ js/api/sources.js
@@ -0,0 +1,40 @@
+import {
+    collection,
+    query,
+    where,
+    getDocs
+}
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import { db } from "../firebase.js";
+
+export async function getSources(objectId) {
+
+    const q = query(
+
+        collection(
+            db,
+            "sources"
+        ),
+
+        where(
+            "parents",
+            "array-contains",
+            objectId
+        )
+
+    );
+
+    const snapshot = await getDocs();
+
+    return snapshot.docs.map(
+        doc => ({
+
+            id: doc.id,
+
+            ...doc.data()
+
+        })
+    );
+
+}
