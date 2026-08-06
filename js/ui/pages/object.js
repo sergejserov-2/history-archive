1a3fa78bc2f980effde9247de99da35a16ac5784
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-06 11:43:53 +0300

    Update object.js

--- js/ui/pages/object.js
+++ js/ui/pages/object.js
@@ -151,7 +151,7 @@ async function loadPage() {
     );
 
         if (ADMIN_MODE) {
-        initAdmin(object, types, objects, photos);
+        initAdmin(object, types, objects, photos, children);
     }
 
 }
