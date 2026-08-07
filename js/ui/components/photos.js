c1aae8293004f840a65fa51148d6d958594a08c1
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-07 12:20:56 +0300

    Update photos.js

--- js/ui/components/photos.js
+++ js/ui/components/photos.js
@@ -97,7 +97,7 @@ export function renderPhotos(
 
                 >
 
-    <img src="../../../icons/edit.svg" class="admin-icon">
+    <img src="icons/edit.svg" class="admin-icon">
 
                 </button>
 
@@ -111,7 +111,7 @@ export function renderPhotos(
 
                 >
 
-    <img src="../../../icons/delete.svg" class="admin-icon">
+    <img src="icons/delete.svg" class="admin-icon">
 
                 </button>
 
