32cb47e889083ddf51be4f6ab599968ecf380131
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-08 16:23:33 +0300

    Update photoViewer.js

--- js/ui/components/photoViewer.js
+++ js/ui/components/photoViewer.js
@@ -27,7 +27,7 @@ export function openPhotoViewer(
 
         <div class="photo-viewer">
 
-            <div class="photo-viewer__image">
+            <div class="photo-viewer__image-area">
 
                 <img
 
