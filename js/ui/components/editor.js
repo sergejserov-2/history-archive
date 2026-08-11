4a5df7acce9f46621d894e438bf1dccebf64fa7f
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-11 23:40:22 +0300

    Update editor.js

--- js/ui/components/editor.js
+++ js/ui/components/editor.js
@@ -9,7 +9,7 @@ import {setupFileEditor, renderFileEditorHTML} from "./editor/file.js";
 import {setupCoverEditor, renderCoverEditorHTML} from "./editor/cover.js";
 import {setupDateModeEditor, renderDateModeEditorHTML} from "./editor/date.js";
 import {setupFieldsEditor, renderFieldsEditorHTML} from "./editor/fields.js";
-import {setupFieldCounters, renderFieldCounterHTML} from "./editor/counters.js";
+import {setupFieldCounters, renderFieldCounterHTML} from "./editor/counters.js";//для совместимости с обжектедитором
 
 // ======================================
 // Render Editor
@@ -152,7 +152,7 @@ export function renderConfiguredEntityEditor(cfg, entity, context) {
 // Compatibility exports
 // ======================================
 
-export {
+export {//для совместимости с обжектедитором
     setupParentsEditor,
     setupFileEditor,
         setupCoverEditor,
