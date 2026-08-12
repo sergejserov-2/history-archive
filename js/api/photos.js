e13b1b8cba08a6e5cd5cd00158ee403a06034e1e
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-12 13:47:08 +0300

    Update photos.js

--- js/api/photos.js
+++ js/api/photos.js
@@ -1,124 +1,51 @@
+// ======================================
+// Photos API
+// ======================================
 import {
     collection,
     query,
     where,
     getDocs,
+    getDoc,
     doc,
     updateDoc,
     addDoc,
     deleteDoc
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import {db} from "../firebase.js";
+// ======================================
+// Get photo
+// ======================================
+export async function getPhoto(id) {
+    if(!id) return null;
+    const snapshot = await getDoc(doc(db, "photos", id));
+    if(!snapshot.exists()) return null;
+    return {id: snapshot.id, ...snapshot.data()};
 }
-from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-
-import { db } from "../firebase.js";
-
+// ======================================
+// Get photos
+// ======================================
 export async function getPhotos(objectId) {
-
-    const q = query(
-
-        collection(
-            db,
-            "photos"
-        ),
-
-        where(
-            "parents",
-            "array-contains",
-            objectId
-        )
-
-    );
-
+    const q = query(collection(db, "photos"), where("parents", "array-contains", objectId));
     const snapshot = await getDocs(q);
-
-    return snapshot.docs.map(
-        doc => ({
-
-            id: doc.id,
-
-            ...doc.data()
-
-        })
-    );
-
+    return snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
 }
-
-export async function updatePhoto(
-
-    id,
-
-    data
-
-){
-
-    await updateDoc(
-
-        doc(
-
-            db,
-
-            "photos",
-
-            id
-
-        ),
-
-        data
-
-    );
-
+// ======================================
+// Update photo
+// ======================================
+export async function updatePhoto(id, data) {
+    await updateDoc(doc(db, "photos", id), data);
 }
-
-export async function createPhoto(
-
-    data
-
-){
-
-    const ref =
-
-        await addDoc(
-
-            collection(
-
-                db,
-
-                "photos"
-
-            ),
-
-            data
-
-        );
-
-    return {
-
-        id: ref.id,
-
-        ...data
-
-    };
-
+// ======================================
+// Create photo
+// ======================================
+export async function createPhoto(data) {
+    const ref = await addDoc(collection(db, "photos"), data);
+    return {id: ref.id, ...data};
 }
-
-export async function deletePhoto(
-
-    id
-
-){
-
-    await deleteDoc(
-
-        doc(
-
-            db,
-
-            "photos",
-
-            id
-
-        )
-
-    );
-
+// ======================================
+// Delete photo
+// ======================================
+export async function deletePhoto(id) {
+    await deleteDoc(doc(db, "photos", id));
 }
