267904d45ecd6f23f54b0d1acaad775d859c9920
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-12 17:28:19 +0300

    Update object.js

--- js/ui/pages/object.js
+++ js/ui/pages/object.js
@@ -194,7 +194,38 @@ console.timeEnd("getAllObjects");
         )
     ]);
 
-    console.timeEnd("LOAD DATA");
+console.timeEnd("LOAD DATA");
+
+console.time("RENDER PAGE");
+await renderPage();
+console.timeEnd("RENDER PAGE");
+
+onAdminStateChanged(
+    async ADMIN_MODE => {
+        pageAdminMode = ADMIN_MODE;
+
+        if(ADMIN_MODE) {
+            initAdmin(
+                pageObject,
+                pageTypes,
+                pageObjects,
+                pagePhotos,
+                pageSources,
+                pageRecords,
+                pageChildren,
+                pageRecordTypes,
+                {
+                    updateObjectBlock,
+                    updateRecordsBlock,
+                    updatePhotosBlock,
+                    updateSourcesBlock
+                }
+            );
+        }
+
+        await restoreModalFromUrl();
+    }
+);
 }
 export async function onPhotoDeleted() {
     await updatePhotosBlock();
