fbe0edca8c56a594915a0b11d6e017b88b7caceb
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-13 09:41:17 +0300

    Update historicalDate.js

--- js/ui/components/editor/historicalDate.js
+++ js/ui/components/editor/historicalDate.js
@@ -351,86 +351,12 @@ export function setupHistoricalDateInput(
         // Validate
         // --------------------------------
 
-validate() {
+        validate() {
 
-    // ----------------------------------
-    // Period
-    // ----------------------------------
-
-    if(mode === "period") {
-
-        const start =
-            dateStartEditor
-                ? dateStartEditor.getValue()
-                : (
-                    dateStartInput
-                        ?.value
-                        .trim() || ""
-                );
-
-        const end =
-            dateEndEditor
-                ? dateEndEditor.getValue()
-                : (
-                    dateEndInput
-                        ?.value
-                        .trim() || ""
-                );
-
-        /*
-         * В периоде обе границы могут быть
-         * пустыми.
-         *
-         * Но если граница заполнена,
-         * она ОБЯЗАНА полностью совпадать
-         * с одним из исторических шаблонов.
-         */
-
-        if(start) {
-
-            if(!isValidHistoricalDate(start)) {
-                return false;
-            }
+            return isValidHistoricalDate(
+                input.value
+            );
         }
-
-        if(end) {
-
-            if(!isValidHistoricalDate(end)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    // ----------------------------------
-    // Single date
-    // ----------------------------------
-
-    if(!dateInput) {
-        return true;
-    }
-
-    const value =
-        dateEditor
-            ? dateEditor.getValue()
-            : dateInput.value.trim();
-
-    /*
-     * Обычная дата обязательна.
-     */
-
-    if(!value) {
-        return false;
-    }
-
-    /*
-     * Здесь тоже требуется полное
-     * совпадение с одним шаблоном.
-     */
-
-    return isValidHistoricalDate(value);
-}
     };
 }
 
