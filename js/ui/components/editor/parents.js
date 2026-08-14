d0968c7b761c08cdea10a462fd820b7e1cb3cee2
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-14 17:04:54 +0300

    Update parents.js

--- js/ui/components/editor/parents.js
+++ js/ui/components/editor/parents.js
@@ -43,7 +43,7 @@ export function setupParentsEditor(
 
     const parentDropdown =
         createDropdown({
-            maxHeight: 360
+            maxHeight: 128
         });
 
     // ======================================
