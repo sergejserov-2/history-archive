c6d909387aeba0340e8c31ea2414ab0ec957b2ee
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-15 19:18:23 +0300

    Create placeholder.js

---
+++ js/ui/components/placeholder.js
@@ -0,0 +1,16 @@
+```js
+// ======================================
+// Loading placeholder
+// ======================================
+
+export function renderLoadingPlaceholder(extraClass=""){
+
+    return `
+        <div class="loading-placeholder ${extraClass}">
+            <div class="loading-placeholder__bg"></div>
+            <div class="loading-placeholder__spinner"></div>
+        </div>
+    `;
+
+}
+```
