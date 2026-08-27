b002c1af716976af53fc9712f4d976c414065a2b
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-27 16:19:29 +0300

    Update resize.js

--- js/ui/animations/resize.js
+++ js/ui/animations/resize.js
@@ -3,8 +3,8 @@
 // Margin based
 // ======================================
 
-const EXPAND_DURATION=420;
-const COLLAPSE_DURATION=420;
+const EXPAND_DURATION=5420;
+const COLLAPSE_DURATION=5420;
 const END_GAP=16.5;
 
 const DEBUG_ANIMATIONS=true;
