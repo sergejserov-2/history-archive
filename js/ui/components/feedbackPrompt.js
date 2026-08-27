19c028e41555f81f4a1b2da792827df4201df48e
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-08-27 23:44:49 +0300

    Update feedbackPrompt.js

--- js/ui/components/feedbackPrompt.js
+++ js/ui/components/feedbackPrompt.js
@@ -1,4 +1,4 @@
-import{setModalUrl,restoreModalFromUrl}from"./modalReload.js";
+import{openModal}from"./modalReload.js";
 
 export function renderFeedbackPrompt(objectId){
     if(!objectId)return"";
@@ -9,15 +9,13 @@ export function renderFeedbackPrompt(objectId){
                 <span class="feedback-prompt__text">
                     У вас есть информация об этом объекте?
                 </span>
-
                 <button
                     class="feedback-prompt__button"
-                    data-feedback-object-id="${objectId}"
+                    data-feedback-object-id="${escapeHTML(objectId)}"
                 >
                     Напишите нам!
                 </button>
             </div>
-
             <button
                 class="feedback-prompt__close"
                 type="button"
@@ -34,33 +32,27 @@ export function initFeedbackPrompt(){
     const prompt=document.querySelector("#feedbackPrompt");
     if(!prompt)return;
 
-    const button=prompt.querySelector(
-        ".feedback-prompt__button"
-    );
-
-    const close=prompt.querySelector(
-        ".feedback-prompt__close"
-    );
+    const button=prompt.querySelector(".feedback-prompt__button");
+    const close=prompt.querySelector(".feedback-prompt__close");
 
     if(button){
         button.onclick=async()=>{
-            const objectId=
-                button.dataset.feedbackObjectId;
-
+            const objectId=button.dataset.feedbackObjectId;
             if(!objectId)return;
-
-            setModalUrl(
-                "feedback",
-                {objectId}
-            );
-
-            await restoreModalFromUrl();
+            await openModal("feedback",{objectId});
         };
     }
 
     if(close){
-        close.onclick=()=>{
-            prompt.remove();
-        };
+        close.onclick=()=>prompt.remove();
     }
+}
+
+function escapeHTML(value=""){
+    return String(value)
+        .replaceAll("&","&amp;")
+        .replaceAll("<","&lt;")
+        .replaceAll(">","&gt;")
+        .replaceAll('"',"&quot;")
+        .replaceAll("'","&#039;");
 }
